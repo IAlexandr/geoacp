862e70f8e3a4e9f1d73735587616f48549cd0233
Alexandr Ivanov <aivanov@informatica.ru>
2014-08-23 15:33:52 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var finder = require('./lib/finder');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.get('/:collection/find', function (req, res) {
+    var expression;
+    var skip;
+    var limit;
+    var sort;
+
+    if (req.query.expression) {
+        try {
+            expression = JSON.parse(req.query.expression);
+        } catch (err) {
+            return res.send(500, err.message);
+        }
+    } else {
+        expression = {};
+    }
+
+
+
+    try {
+        skip = req.query.skip ? parseInt(req.query.skip) : 0;
+        limit = req.query.limit ? parseInt(req.query.limit) : 10;
+        sort = req.query.sort ? JSON.parse(req.query.sort) : {};
+    } catch (err) {
+        return res.send(500, err.message);
+    }
+
+
+
+    finder.find(req.params.collection, {expression: expression, skip: skip, limit: limit, sort: sort}, function (err, result) {
+        if (err) {
+            return res.json(500, err);
+        }
+        return res.json(200, result);
+    });
+});
+app.get('/:collection/:objectId', function (req, res) {
+    finder.findByObjectId(req.params.objectId, req.params.collection, function (err, result) {
+        if (err) {
+            return res.json(500, err);
+        }
+        return res.json(200, result);
+    });
+});
+
+
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
