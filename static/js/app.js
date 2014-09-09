ff8bf8c670801e6bfcc593351fc0371e292bd203
Alexandr Ivanov <aivanov@informatica.ru>
2014-09-09 16:18:50 +0400

    рефакторинг

--- static/js/app.js
+++ static/js/app.js
@@ -5,4 +5,146 @@ angular.module('ngFias', ['ngResource'])
     })
     .controller("MapController", function ($scope) {
 
+        /* настройки отображения полей
+         * Обязательные поля:
+         *   > show: (Boolean) показывать поле
+         *   > alias: (String) Подпись поля
+         *   > controlType: ('text', 'textarea', 'num', 'select', 'checkbox') тип поля
+         *   > nullable: (Boolean) пустая строка
+         *   > editable: (Boolean) редактирование значения
+         *   > order: (Integer) порядок отображения
+         * Необязательные поля:
+         *   > length: (Integer) макс. допустимое кол-во символов
+         *   > rows: (Integer) (используется в 'textarea') кол-во строк
+         *   > trueValue: 'True' (любое значение) (используется в 'checkbox') значение в true состоянии
+         *   > falseValue: 'False' (любое значение) (используется в 'checkbox') значение в false состоянии
+         */
+        $scope.formDefinition = {
+            'OBJECTID': {
+                alias: 'OBJECTID',
+                show: false,
+                controlType: 'text',
+                nullable: false,
+                editable: false,
+                order: 0
+            },
+            'STREET': {
+                alias: 'Улица',
+                show: true,
+                controlType: 'text',
+                length: 10,
+                nullable: false,
+                editable: true,
+                order: 1
+            },
+            'HOUSE': {
+                alias: 'Дом',
+                show: true,
+                controlType: 'num',
+                nullable: false,
+                editable: true,
+                order: 2
+            },
+            'SYMTYPE': {
+                domain: {
+                    "name": "PTANNOTYPE",
+                    "type": "codedValue",
+                    "codedValues": [
+                        { "name": "Прочее", "code": 0 },
+                        { "name": "Устаревшие данные", "code": 1 },
+                        { "name": "Отсутствует объект", "code": 2 },
+                        { "name": "К сведению", "code": 3 }
+                    ]
+                },
+                alias: 'Тип2',
+                show: true,
+                controlType: 'select',
+                nullable: true,
+                editable: true,
+                order: 3
+            },
+            'ANNO': {
+                alias: 'Примечание',
+                show: true,
+                controlType: 'textarea',
+                length: 40,
+                rows: 2,
+                nullable: false,
+                editable: true,
+                order: 4
+            },
+            'CHECK': {
+                alias: 'Проверен',
+                show: true,
+                controlType: 'checkbox',
+                length: 1,
+                trueValue: 'True',
+                falseValue: 'False',
+                nullable: false,
+                editable: true,
+                order: 5
+            }
+        };
+
+        $scope.featureAttrs = {
+            OBJECTID: 1,
+            STREET: "Ленина",
+            HOUSE: 13,
+            ANNO: 'Информация по улице',
+            SYMTYPE: 1,
+            CHECK: "False"
+        };
+
+
+        // поля
+        $scope.fields = [
+            {
+                "name": "OBJECTID",
+                "type": "esriFieldTypeOID",
+                "alias": "OBJECTID",
+                "editable": false,
+                "nullable": false
+            },
+            {
+                "name": "STREET",
+                "type": "esriFieldTypeString",
+                "alias": "Улица",
+                "length": 10,
+                "editable": true,
+                "nullable": true
+            },
+            {
+                "name": "SYMTYPE",
+                "type": "esriFieldTypeInteger",
+                "alias": "Тип",
+                "editable": true,
+                "nullable": true,
+                "domain": {
+                    "name": "PTANNOTYPE",
+                    "type": "codedValue",
+                    "codedValues": [
+                        { "name": "Прочее", "code": 0 },
+                        { "name": "Устаревшие данные", "code": 1 },
+                        { "name": "Отсутствует объект", "code": 2 },
+                        { "name": "К сведению", "code": 3 }
+                    ]
+                }
+            },
+            {
+                "name": "ANNO",
+                "type": "esriFieldTypeString",
+                "alias": "Примечание",
+                "length": 1000,
+                "editable": true,
+                "nullable": true
+            },
+            {
+                "name": "CHECK",
+                "type": "esriFieldTypeInteger",
+                "alias": "Проверен",
+                "length": 1,
+                "editable": true,
+                "nullable": false
+            }
+        ];
     });
--- static/js/form_directives.js
+++ static/js/form_directives.js
@@ -1,166 +1,20 @@
 angular.module('ngFias')
-    .directive("editForm", function ($compile) {
+    .directive("editForm", function () {
         return {
-            restrict: "E",
             templateUrl: "views/templates/form/edit_form.html",
+            scope: {
+                model: '=gwModel',
+                formDefinition: '=formDefinition'
+            },
             link: function ($scope, element, attr) {
-
-                // тестовый объект
-                $scope.oldObj = {
-                    OBJECTID: 1,
-                    STREET: "Ленина",
-                    HOUSE: 13,
-                    ANNO: 'Информация по улице',
-                    SYMTYPE: 1,
-                    CHECK: "False"
-                };
-                // объект для изменения
-                $scope.res = angular.copy($scope.oldObj);
-
-                /* настройки отображения полей
-                * Обязательные поля:
-                *   > show: (Boolean) показывать поле
-                *   > alias: (String) Подпись поля
-                *   > controlType: ('text', 'textarea', 'num', 'select', 'checkbox') тип поля
-                *   > nullable: (Boolean) пустая строка
-                *   > editable: (Boolean) редактирование значения
-                *   > order: (Integer) порядок отображения
-                * Необязательные поля:
-                *   > length: (Integer) макс. допустимое кол-во символов
-                *   > rows: (Integer) (используется в 'textarea') кол-во строк
-                *   > trueValue: 'True' (любое значение) (используется в 'checkbox') значение в true состоянии
-                *   > falseValue: 'False' (любое значение) (используется в 'checkbox') значение в false состоянии
-                */
-                $scope.formDefinition = {
-                    'OBJECTID': {
-                        alias: 'OBJECTID',
-                        show: false,
-                        controlType: 'text',
-                        nullable: false,
-                        editable: false,
-                        order: 0
-                    },
-                    'STREET': {
-                        alias: 'Улица',
-                        show: true,
-                        controlType: 'text',
-                        length: 10,
-                        nullable: false,
-                        editable: true,
-                        order: 1
-                    },
-                    'HOUSE': {
-                        alias: 'Дом',
-                        show: true,
-                        controlType: 'num',
-                        nullable: false,
-                        editable: true,
-                        order: 2
-                    },
-                    'SYMTYPE': {
-                        domain: true,
-                        alias: 'Тип2',
-                        show: true,
-                        controlType: 'select',
-                        nullable: true,
-                        editable: true,
-                        order: 3
-                    },
-                    'ANNO': {
-                        alias: 'Примечание',
-                        show: true,
-                        controlType: 'textarea',
-                        length: 40,
-                        rows: 2,
-                        nullable: false,
-                        editable: true,
-                        order: 4
-                    },
-                    'CHECK': {
-                        alias: 'Проверен',
-                        show: true,
-                        controlType: 'checkbox',
-                        length: 1,
-                        trueValue: 'True',
-                        falseValue: 'False',
-                        nullable: false,
-                        editable: true,
-                        order: 5
-                    }
-                };
-
-                // поля
-                $scope.fields = [
-                    {
-                        "name": "OBJECTID",
-                        "type": "esriFieldTypeOID",
-                        "alias": "OBJECTID",
-                        "editable": false,
-                        "nullable": false
-                    },
-                    {
-                        "name": "STREET",
-                        "type": "esriFieldTypeString",
-                        "alias": "Улица",
-                        "length": 10,
-                        "editable": true,
-                        "nullable": true
-                    },
-                    {
-                        "name": "SYMTYPE",
-                        "type": "esriFieldTypeInteger",
-                        "alias": "Тип",
-                        "editable": true,
-                        "nullable": true,
-                        "domain": {
-                            "name": "PTANNOTYPE",
-                            "type": "codedValue",
-                            "codedValues": [
-                                { "name": "Прочее", "code": 0 },
-                                { "name": "Устаревшие данные", "code": 1 },
-                                { "name": "Отсутствует объект", "code": 2 },
-                                { "name": "К сведению", "code": 3 }
-                            ]
-                        }
-                    },
-                    {
-                        "name": "ANNO",
-                        "type": "esriFieldTypeString",
-                        "alias": "Примечание",
-                        "length": 1000,
-                        "editable": true,
-                        "nullable": true
-                    },
-                    {
-                        "name": "CHECK",
-                        "type": "esriFieldTypeInteger",
-                        "alias": "Проверен",
-                        "length": 1,
-                        "editable": true,
-                        "nullable": false
-                    }
-                ];
-
-
-                $scope.applyEdits = function () {
-                    $scope.oldObj = angular.copy($scope.res);
-                };
-                $scope.reset = function () {
-                    $scope.res = angular.copy($scope.oldObj);
-                };
-
-                $scope.equalsCheck = function () {
-                    return angular.equals($scope.res, $scope.oldObj);
-                }
             }
         }
     })
-    .directive("editFieldText", function ($compile) {
+    .directive("editFieldText", function () {
         return {
-            restrict: "E",
             templateUrl: "views/templates/form/edit_field_text.html",
             scope: {
-                res: '=resObj',
+                model: '=gwModel',
                 fd: '=fdObj'
             },
             link: function ($scope, element) {
@@ -170,10 +24,9 @@ angular.module('ngFias')
     })
     .directive("editFieldNum", function () {
         return {
-            restrict: "E",
             templateUrl: "views/templates/form/edit_field_num.html",
             scope: {
-                res: '=resObj',
+                model: '=gwModel',
                 fd: '=fdObj'
             },
             link: function ($scope, element) {
@@ -184,9 +37,8 @@ angular.module('ngFias')
     })
     .directive("editFieldTextarea", function () {
         return {
-            restrict: "E",
             scope: {
-                res: '=resObj',
+                model: '=gwModel',
                 fd: '=fdObj'
             },
             templateUrl: "views/templates/form/edit_field_textarea.html",
@@ -197,31 +49,20 @@ angular.module('ngFias')
     })
     .directive("editFieldSelect", function () {
         return {
-            restrict: "E",
             scope: {
-                fieldsTest: '=fieldsTest',
-                res: '=resObj',
+                model: '=gwModel',
                 fd: '=fdObj'
             },
             templateUrl: "views/templates/form/edit_field_select.html",
             link: function ($scope, element, attr) {
-                if ($scope.fd.domain) {
 
-                    var field = _.where($scope.fieldsTest, {name: $scope.fd.formDefinitionKey});
-                    if (field.length > 0 && field[0].domain.codedValues) {
-                        $scope.values = field[0].domain.codedValues;
-                    } else {
-                        //todo
-                    }
-                }
             }
         }
     })
     .directive("editFieldCheckbox", function () {
         return {
-            restrict: "E",
             scope: {
-                res: '=resObj',
+                model: '=gwModel',
                 fd: '=fdObj'
             },
             templateUrl: "views/templates/form/edit_field_checkbox.html",
